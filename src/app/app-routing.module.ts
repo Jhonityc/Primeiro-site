c7982627daa19fc6a0b50424574c9b816d05c1a2
Jhoni Tim Yoing Chen <jhonityc@gmail.com>
2022-07-06 22:16:43 -0300

    Primeiro commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './page/about/about.component';
+import { ContactComponent } from './page/contact/contact.component';
+import { HomeComponent } from './page/home/home.component';
+import { MenuComponent } from './page/menu/menu.component';
+
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'menu', component:MenuComponent},
+  {path:'about', component:AboutComponent},
+  {path:'contact', component:ContactComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FooterComponent } from './sharepage/footer/footer.component';
+import { MenuComponent } from './page/menu/menu.component';
+import { HomeComponent } from './page/home/home.component';
+import { AboutComponent } from './page/about/about.component';
+import { ContactComponent } from './page/contact/contact.component';
+import { NavbarComponent } from './sharepage/navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,    
+    FooterComponent,
+    MenuComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
